fd87bfb8f356da5b83fb87ba9f11e196d3f19a33
David1337 <davidmolish@gmail.com>
2018-12-19 20:15:06 -0500

    Update item.lua

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -1,1 +1,17 @@
+local phil = table.deepcopy(data.raw.demo-item["iron-stick"])
 
+phil.name = "Philosopher's Stone"
+phil.icons = {
+  {
+    icon = phil.icon,
+    tint = {r=248,g=24,b=148,a=150}
+  },
+}
+
+local recipe = table.deepcopy(data.raw.recipe["iron-stick"])
+recipe.enabled = true
+recipe.name = "Philosopher's Stone"
+recipe.ingredients = {{"sulfur",1},{"plastic-bar",1}}
+recipe.result = "Philosopher's Stone"
+
+data:extend{phil,recipe}
