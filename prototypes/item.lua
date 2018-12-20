abb6460eb8d2d3487af372bc46b98a95a391eee0
devilsmentor <bobhaxme@hotmail.com>
2018-12-19 21:27:35 -0500

    Update item.lua

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -4,7 +4,6 @@ data:extend(
     type = "item",
     name = "philosphers-stone",
     icon = "__base__/graphics/icons/stone.png",
-      tint={r=248,g=24,b=148,a=150},
     icon_size = 32,
     flags = {"goes-to-main-inventory"},
     subgroup = "intermediate-product",
