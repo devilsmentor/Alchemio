01923c4730128aff5dcca961366e3a251b1ff55c
Neil Kenney <nfkenney@umich.edu>
2018-12-19 21:14:52 -0500

    added strongbox

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -1,3 +1,4 @@
+--Philosopher's Stone
 local philStone = table.deepcopy(data.raw.item["iron-stick"])
 
 philStone.name = "philosophers-stone"
@@ -8,10 +9,28 @@ philStone.icons = {
   },
 }
 
-local recipe = table.deepcopy(data.raw.recipe["iron-stick"])
-recipe.enabled = true
-recipe.name = "Philosopher's Stone"
-recipe.ingredients = {{"sulfur",1},{"plastic-bar",1}}
-recipe.result = "philosophers-stone"
+local philStoneRecipe = table.deepcopy(data.raw.philStoneRecipe["iron-stick"])
+philStoneRecipe.enabled = true
+philStoneRecipe.name = "Philosopher's Stone"
+philStoneRecipe.ingredients = {{"sulfur",1},{"plastic-bar",1}}
+philStoneRecipe.result = "philosophers-stone"
 
-data:extend{philStone,recipe}
+data:extend{philStone,philStoneRecipe}
+
+--Philosopher's StrongBox
+local philStrongBox = table.deepcopy(data.raw.item["iron-chest"])
+
+philStrongBox.name = "philosophers-strongbox"
+philStrongBox.icons = {
+  {
+    icon = philStrongBox.icon,
+    tint = {r=248,g=24,b=148,a=150}
+  },  
+}
+local philBoxRecipe = table.deepcopy(data.raw.philBoxRecipe["iron-chest"])
+philBoxRecipe.enabled = true
+philBoxRecipe.name = "Philosopher's StrongBox"
+philBoxRecipe.ingredients = {{"sulfur",1},{"plastic-bar",1}}
+philBoxRecipe.result = "philosophers-strongbox"
+
+data:extend{philStone,philBoxRecipe}
