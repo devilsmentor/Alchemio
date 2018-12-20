ed4bbd3315c0619ba3c4e3f98a9de01ba855c544
David1337 <davidmolish@gmail.com>
2018-12-19 21:12:47 -0500

    Create recipe.lua

---
+++ prototypes/recipe.lua
@@ -0,0 +1,11 @@
+data:extend(
+{
+  {
+    type = "recipe",
+    name = "philosophers-stone"
+    enabled = false,
+    ingredients = {{"sulfur",1},{"plastic-bar",1}}
+    result = "philosophers-stone"
+  }
+}
+)
