de5e516e19c85cfe2d8affd6dfb6e2dc4157f260
devilsmentor <bobhaxme@hotmail.com>
2018-12-19 23:17:52 -0500

    Update recipe.lua

--- prototypes/recipe/recipe.lua
+++ prototypes/recipe/recipe.lua
@@ -1,16 +1,36 @@
 data:extend(
 {
-  {
-    type = "recipe",
-    name = "philosophers-stone",
-    ingredients = {{"sulfur",1},{"plastic-bar",1}},
-    result = "philosophers-stone",
+    {
+        type = "recipe",
+        name = "philosophers-stone",
+        ingredients = {{"sulfur",1},{"plastic-bar",1}},
+        result = "philosophers-stone",
+    },
+    {
+        type = "recipe",
+        name = "alchemy-science-pack",
+        ingredients = {{"philosophers-stone",3},{"sulfur",2}},
+        result = "alchemy-science-pack"
+    },
+    {
+        type = "recipe",
+        name = "convert-copper-to-iron",
+        icon = "__base__/graphics/icons/iron-plate.png"
+        ingredients = {{"copper-plate",10},{"philosophers-stone",1}},
+        result = {
+            {type="item", name="iron-plate", amount=1},
+            {type="item", name="philosophers-stone", amount=1},
+        }
   },
-  {
-    type = "recipe",
-    name = "alchemy-science-pack",
-    ingredients = {{"philosophers-stone",3},{"sulfur",2}},
-    result = "alchemy-science-pack"
-  }
+    {
+        type = "recipe",
+        name = "convert-iron-to-copper",
+        icon = "__base__/graphics/icons/copper-plate.png"
+        ingredients = {{"iron-plate",1},{"philosophers-stone",1}},
+        result = {
+            {type="item", name="copper-plate", amount=10},
+            {type="item", name="philosophers-stone", amount=1},
+        }
+  },
 }
 )
