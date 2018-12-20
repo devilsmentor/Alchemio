16811458e667ca340a1b31fb2ba5984d4d988a9a
devilsmentor <bobhaxme@hotmail.com>
2018-12-20 00:08:53 -0500

    missing }

--- prototypes/technology/technology.lua
+++ prototypes/technology/technology.lua
@@ -1,40 +1,42 @@
 data:extend(
 {
-    type = "technology",
-    name = "basic-alchemy",
-    icon_size = 128,
-    icon = "__base__/graphics/technology/steel-processing.png",
-    effects =
     {
+        type = "technology",
+        name = "basic-alchemy",
+        icon_size = 128,
+        icon = "__base__/graphics/technology/steel-processing.png",
+        effects =
         {
-          type = "unlock-recipe",
-          recipe = "philosophers-stone"
+            {
+              type = "unlock-recipe",
+              recipe = "philosophers-stone"
+            },
+            {
+              type = "unlock-recipe",
+              recipe = "alchemy-science-pack"
+            },
+            {
+              type = "unlock-recipe",
+              recipe = "convert-copper-to-iron"
+            },
+            {
+              type = "unlock-recipe",
+              recipe = "convert-iron-to-copper"
+            }
         },
+        prerequisites = {"sulfur-processing"},
+        unit =
         {
-          type = "unlock-recipe",
-          recipe = "alchemy-science-pack"
-        },
-        {
-          type = "unlock-recipe",
-          recipe = "convert-copper-to-iron"
-        },
-        {
-          type = "unlock-recipe",
-          recipe = "convert-iron-to-copper"
+            count = 100,
+            ingredients = {
+                {"science-pack-1", 1},
+                {"science-pack-2", 1},
+                {"science-pack-3", 1}
+            },
+            time = 10
+            },
+            order = "d-d"
         }
-    },
-    prerequisites = {"sulfur-processing"},
-    unit =
-    {
-        count = 100,
-        ingredients = {
-            {"science-pack-1", 1},
-            {"science-pack-2", 1},
-            {"science-pack-3", 1}
-        },
-        time = 10
-        },
-        order = "d-d"
     }
 }
 )
