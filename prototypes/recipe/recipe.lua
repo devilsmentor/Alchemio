d60ce07a8a93ba561f6c5a30697c63e1afd3a6a1
Neil Kenney <nfkenney@umich.edu>
2018-12-19 21:44:40 -0500

    testing?

---
+++ prototypes/recipe/recipe.lua
@@ -0,0 +1,15 @@
+data:extend(
+{
+  {
+    type = "recipe",
+    name = "philosophers-stone",
+    ingredients = {{"sulfur",1},{"plastic-bar",1}},
+    result = "philosophers-stone",
+  },
+  {
+    type = "recipe",
+    name = "science-pack-alchemy",
+    ingredients = {{"sulfur",1},{"plastic-bar",1}},
+  }
+}
+)
