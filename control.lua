fd8adc0da20602896f54a38392a960ea6ea438d9
devilsmentor <bobhaxme@hotmail.com>
2018-12-20 22:57:55 -0500

    Print once for each cosmic-strongbox in the world

--- control.lua
+++ control.lua
@@ -1,1 +1,15 @@
+--[[
+~~ References:
+https://lua-api.factorio.com/latest/LuaForce.html#LuaForce.get_entity_count
+https://lua-api.factorio.com/latest/LuaGameScript.html#LuaGameScript.get_entity_by_tag
 
+]]--
+
+script.on_event({defines.events.on_tick},
+    function (e)
+        if LuaForce.get_entity_count("cosmic-strongbox") == 0 then break end --back out early if there are no strongboxes to update
+        for index, entity in pairs(game.get_entity_by_tag("cosmic-strongbox")) do
+            print("Entity found!")
+        end
+    end
+)
