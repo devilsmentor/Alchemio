6ae759b2b7b76da798158af7f49d80e8d820b689
devilsmentor <bobhaxme@hotmail.com>
2018-12-20 23:21:26 -0500

    print the contents of get_entity_by_tag just to see what we get

--- control.lua
+++ control.lua
@@ -9,7 +9,7 @@ script.on_event({defines.events.on_tick},
     function (e)
         --if LuaForce.get_entity_count("cosmic-strongbox") == 0 then return false end --back out early if there are no strongboxes to update
         
-        game.print("TEST")
+        game.print(game.get_entity_by_tag("cosmic-strongbox"))
         
         if game.get_entity_by_tag("cosmic-strongbox")==nil then return false end
         for index, entity in pairs(game.get_entity_by_tag("cosmic-strongbox")) do
