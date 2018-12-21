01a7170f360d011205fd8f5236ce9ce74a90e3b5
devilsmentor <bobhaxme@hotmail.com>
2018-12-20 23:44:40 -0500

    testing events

--- control.lua
+++ control.lua
@@ -2,18 +2,12 @@
 ~~ References:
 https://lua-api.factorio.com/latest/LuaForce.html#LuaForce.get_entity_count
 https://lua-api.factorio.com/latest/LuaGameScript.html#LuaGameScript.get_entity_by_tag
-
+https://wiki.factorio.com/Tutorial:Modding_FAQ#How_do_I_store_information_with_an_entity.2C_like_integers_or_booleans.3F
 ]]--
 
-script.on_event({defines.events.on_tick},
+script.on_event({defines.events.on_built_entity},
     function (e)
-        --if LuaForce.get_entity_count("cosmic-strongbox") == 0 then return false end --back out early if there are no strongboxes to update
-        
-        game.print(game.get_entity_by_tag("cosmic-strongbox"))
-        
-        if game.get_entity_by_tag("cosmic-strongbox")==nil then return false end
-        for index, entity in pairs(game.get_entity_by_tag("cosmic-strongbox")) do
-            game.print("Entity found!")
+        game.print("someone made an entity!")
         end
 
     end
