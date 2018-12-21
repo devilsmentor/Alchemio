2686c5029161afa71c8db5670eac995642ff21d7
devilsmentor <bobhaxme@hotmail.com>
2018-12-21 18:59:22 -0500

    optimization of condenser code

--- control.lua
+++ control.lua
@@ -5,29 +5,75 @@ https://lua-api.factorio.com/latest/LuaGameScript.html#LuaGameScript.get_entity_
 https://wiki.factorio.com/Tutorial:Modding_FAQ#How_do_I_store_information_with_an_entity.2C_like_integers_or_booleans.3F
 ]]--
 
-script.on_event({defines.events.on_tick}, --todo: don't go through each entity every tick! change this event to check chest inventory updates or something
+local exchange_rates = {["iron-plate"] = 1, ["copper-plate"] = 10} --todo: store in a .json and read on load, then balance the json accordingly
+local condensers = {}
+local condenser_count = 0
+
+----
+
+function cosmicCondenserCreated(condenser)
+    condensers[condenser_count+1] = condenser
+    condenser_count = condenser_count+1
+end
+
+script.on_event({defines.events.on_built_entity}, 
     function (e)
-        local exchange_rates = {["iron-plate"] = 1, ["copper-plate"] = 10} --todo: store in a .json and read on load, then balance the json accordingly
+        if e.created_entity.name=="cosmic-condenser" then
+            cosmicCondenserCreated(e.created_entity)
+        end
+    end
+)
 
-        for chunk in game.surfaces[1].get_chunks() do --For each chunk in the game, this might be slow. Look for other methods of getting entities in an array
-            local entities = game.surfaces[1].find_entities_filtered{name="cosmic-strongbox", area={{chunk.x*32, chunk.y*32}, {(chunk.x+1)*32, (chunk.y+1)*32}}} 
-            for _, entity in pairs(entities) do
+script.on_event({defines.events.on_robot_built_entity}, 
+    function (e)
+        if e.created_entity.name=="cosmic-condenser" then
+            cosmicCondenserCreated(e.created_entity)
+        end
+    end
+)
+        
+----
 
-                local inv = entity.get_inventory(defines.inventory.chest) --Pulls the chest's inventory. We can modify this variable and the game will reflect whatever changes are made. 
-                local dict_items = inv.get_contents() --Converts the inventory to a dictionary. This dict is in format {name, count}. 
-                    
-                for item_name, amount_found in pairs(dict_items) do --iterate through each item
-                    if (exchange_rates[item_name]~=nil) then --if item has value...
-                        inv.remove({name=item_name,count=amount_found}) --remove that item
-                        inv.insert({name="emc",count=amount_found*exchange_rates[item_name]}) --replace it with the proper number of EMC
-                            
-                        --Note: with this method, we cannot have fractions of emc, which might be bad or good, but it probably won't matter in the long run. 
-                            
-                    end
+function cosmicCondenserDestroyed(condenser_to_remove)
+    
+    table.remove(condenser) --apparently, this might cause slowdowns, so this needs testing
+    condenser_count = condenser_count-1
+end
+
+script.on_event({defines.events.on_player_mined_entity}, 
+    function (e)
+        if e.entity.name=="cosmic-condenser" then
+            cosmicCondenserDestroyed(e.entity)
+        end
+    end
+)
+
+script.on_event({defines.events.on_robot_mined_entity}, 
+    function (e)
+        if e.entity.name=="cosmic-condenser" then
+            cosmicCondenserDestroyed(e.entity)
+        end
+    end
+)
+
+script.on_event({defines.events.on_tick}, 
+    function (e)
+        
+        if condenser_count == 0 then return true end --No need to continue if there are no condensers
+        
+        for _, condenser in pairs(condensers) do
+            
+            local inv = condenser.get_inventory(defines.inventory.chest) --Pulls the chest's inventory. We can modify this variable and the game will reflect whatever changes are made. 
+            local items = inv.get_contents() --Converts the inventory to a dictionary. This dict is in format {name, count}. 
+            
+            for item_name, amount in pairs(items) do --iterate through each item
+                if (exchange_rates[item_name]~=nil) then --if item has value...
+                    inv.remove({name=item_name,count=amount}) --remove that item
+                    inv.insert({name="emc",count=amount*exchange_rates[item_name]}) --replace it with the proper number of EMC
                 end
-                --todo proofread, add electricity need
-
             end
+            --todo: add electricity need?
+
         end
 
     end
