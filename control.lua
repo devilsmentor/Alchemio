3c5713043404d78404e49df60c5b60515ceb6ba6
devilsmentor <bobhaxme@hotmail.com>
2018-12-20 23:17:14 -0500

    changed print to game.print

--- control.lua
+++ control.lua
@@ -8,6 +8,11 @@ https://lua-api.factorio.com/latest/LuaGameScript.html#LuaGameScript.get_entity_
 script.on_event({defines.events.on_tick},
     function (e)
         --if LuaForce.get_entity_count("cosmic-strongbox") == 0 then return false end --back out early if there are no strongboxes to update
-        error("Test!")
+        if game.get_entity_by_tag("cosmic-strongbox")==nil then return false end
+        for index, entity in pairs(game.get_entity_by_tag("cosmic-strongbox")) do
+            game.print("Entity found!")
+        end
+        game.print(game.get_entity_by_tag("cosmic-strongbox"))
+
     end
 )
