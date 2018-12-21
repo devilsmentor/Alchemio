34d6cc8a4125b80ccb62af97069d21c381fb60aa
devilsmentor <bobhaxme@hotmail.com>
2018-12-20 22:32:05 -0500

    enabled entity.lua again

--- data.lua
+++ data.lua
@@ -1,4 +1,4 @@
 require("prototypes.item.item") 
 require("prototypes.recipe.recipe")
 require("prototypes.technology.technology")
---require("prototypes.entity.entity")
+require("prototypes.entity.entity")
