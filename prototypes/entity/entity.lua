052824719c05e70ca7105b74848ce5ceba21cd14
Neil Kenney <nfkenney@umich.edu>
2018-12-21 20:56:57 -0500

    added transmutation-station to entity.lua

--- prototypes/entity/entity.lua
+++ prototypes/entity/entity.lua
@@ -40,6 +40,44 @@ data:extend(
         circuit_wire_connection_point = circuit_connector_definitions["chest"].points,
         circuit_connector_sprites = circuit_connector_definitions["chest"].sprites,
         circuit_wire_max_distance = default_circuit_wire_max_distance
+    },
+    {
+        type = "production-machine",
+        name = "transmutation-station",
+        icon = "__Alchemio__/graphics/chemical-plant.png",
+        icon_size = 32,
+        flags = {"placeable-neutral", "player-creation"},
+        minable = {mining_time = 1, result = "transmutation-station"},
+        max_health = 350,
+        corpse = "small-remnants",
+        open_sound = { filename = "__base__/sound/machine-open.ogg", volume = 0.85 },
+    close_sound = { filename = "__base__/sound/machine-close.ogg", volume = 0.75 },
+        resistances =
+        {
+          {
+            type = "fire",
+            percent = 80
+          },
+          {
+            type = "impact",
+            percent = 30
+          }
+        },
+        collision_box = {{-0.35, -0.35}, {0.35, 0.35}},
+        selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
+        fast_replaceable_group = "assembling-machine",
+        vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
+        picture =
+        {
+          filename = "__Alchemio__/graphics/chemical-plant.png",
+          priority = "extra-high",
+          width = 32,
+          height = 32,
+          shift = {0, 0}
+        },
+        circuit_wire_connection_point = circuit_connector_definitions["chest"].points,
+        circuit_connector_sprites = circuit_connector_definitions["chest"].sprites,
+        circuit_wire_max_distance = default_circuit_wire_max_distance
     }
 }
 )
